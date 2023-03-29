6d28b8aae8227e16ec3d689db87299d175641211
Adam Young <adamyoungy678@gmail.com>
2023-03-29 16:49:22 +0100

    deleted function call

--- manualtest.php
+++ manualtest.php
@@ -8,10 +8,10 @@ if ($result !=0){
     echo 'Invalid operation';
 }
 
-$result = multiplyByTwo(-1);
 
-try {$result = multiplyByTwo( -1);
-}
-catch (InvalidArgumentException $exception){
-    echo 'Invalid operation';
+try {
+    $result = multiplyByTwo( -1);
+    echo 'Invalid operation with argument -1';
+} catch (InvalidArgumentException $exception){
+    echo 'valid operation with argument -1';
 }
