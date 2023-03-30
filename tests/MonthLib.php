ca639a1fabed35b8c2736a282654c68dc4d268cc
Adam Young <adamyoungy678@gmail.com>
2023-03-30 11:32:42 +0100

    created name of month test function

--- tests/MonthLib.php
+++ tests/MonthLib.php
@@ -7,5 +7,10 @@ use PHPUnit\Framework\TestCase;
 
 class MonthLib extends TestCase
 {
+    // Every method in a test file must have the prefix 'test'
 
+    public function testNameOfMonth_Given1ReturnJanuary()
+    {
+
+    }
 }
